34a1fcb0967cd954ef26778b23f58c3452377450
francescalovellread <82885897+francescalovellread@users.noreply.github.com>
2023-07-12 18:01:18 +0100

    Add files via upload

---
+++ BASELINE/BASELINE/baseline_RUN.m
@@ -0,0 +1,105 @@
+%% Code for Fig 3C: computing the baseline EDP for a range of sample sizes and intervals
+% Francesca Lovell-Read (francesca.lovell-read@merton.ox.ac.uk)
+% Version of: Thursday 4th August 2022
+
+%% -----------------------------------------------------------------------------------------------
+% This code uses the functions 'runSpread_1' and 'runSampling_1' to compute the
+% baseline EDP across a specified range of sample sizes and intervals. 
+
+function baseline_RUN(ID)
+
+%% ------------------------------------------------------------------------
+% DEFINE MODEL PARAMETERS
+
+% Population size (P=P_C)
+P = 1000; 
+% Transmission coefficient for 'Detectable' crops (beta_C)
+r=0.05; beta = r/P;
+% Transmission scaling factor for 'Undetectable' crops (epsilon_C)
+epsilon = 0.015; 
+% Duration of crop 'Undetectable' period (gamma_C)
+gamma = 452;
+% Initial numbers of 'Detectable' and 'Undetectable' crops
+D0 = 0; U0 = 1; 
+
+numYears = 20;
+
+% Vector of sample sizes to consider
+sampleSizeVec = 25:5:200;
+% Vector of sample intervals to consider
+sampleIntervalVec = 30:5:150;
+
+% Number of spread simulations to perform
+numSims = 250; 
+% Number of sampling simulations to perform
+numRuns = 250;
+% Maximum run time for spread and sampling simulations
+tFinal = 8000; 
+% Specify whether to display progress messages
+progress = "no";
+% Define file path for save location
+savePath = './results/';
+% Define random number generator
+rng('shuffle');
+
+%% ------------------------------------------------------------------------
+% SPECIFY DISTRIBUTION OF START DAYS (COMMENT OUT AS APPROPRIATE)
+
+% Seasonal transmission rate (according to fitted vector numbers)
+C1 = 0.007265; a = 0.0004574; g0 = 0.064913; tem = 80;
+day_vals = 1:1:365;
+Pvec = C1*exp(g0*(day_vals-tem)-(a/2)*(day_vals-tem).^2);
+omega = Pvec/mean(Pvec);
+pdf = omega/sum(omega);
+cdf = cumtrapz(pdf);
+r = rand(1,numSims);
+
+startDays = floor(interp1(cdf,day_vals,r)+1);
+omega1 = omega;
+
+% Seasonal symptom development rate (according to temperature)
+temp = [9 9 11 14 19 23 26 26 22 18 13 10];
+temp(temp<17)=0;
+temp = temp-min(temp); temp = temp/mean(temp);
+temp = interp1(1:1:12,temp,linspace(1,12,365));
+
+omega2 = temp;
+
+%% ------------------------------------------------------------------------
+% END USER INPUT
+
+%% ------------------------------------------------------------------------
+% EXTRACT SAMPLE SIZE AND SAMPLE INTERVAL FROM LISTS
+
+[p,q] = meshgrid(sampleSizeVec,sampleIntervalVec);
+pairs = [p(:) q(:)];
+pairs_choose = pairs(ID,:);
+sampleSize = pairs_choose(1);
+sampleInterval = pairs_choose(2);
+
+%% ------------------------------------------------------------------------
+% GENERATE EPIDEMIC CURVES
+
+simData = runTimeSpread_VSDR_1(P,D0,U0,beta,epsilon,gamma,omega1,omega2,numYears,numSims,startDays,tFinal,progress);
+
+%% ------------------------------------------------------------------------
+% PERFORM SAMPLING SCHEME FOR RANGE OF SAMPLE SIZES AND INTERVALS
+
+[~, EDP, ~, ~, ~] = runWindowSampling_1(simData,numRuns,P,0,364,sampleSize,sampleInterval,tFinal,progress);
+results = [sampleSize, sampleInterval, EDP];
+    
+%% ------------------------------------------------------------------------
+% WRITE RESULTS TO .TXT FILES
+
+% Make new directory to store results
+mkdir(savePath)
+
+T = table(P,beta,epsilon,gamma,D0,U0,numSims,numRuns,tFinal);
+if ID==1
+    writetable(T,[savePath 'params.txt'],'Delimiter','tab');
+end
+
+filename = sprintf('results_%d.txt',ID);
+writematrix(results,[savePath filename],'Delimiter','tab');
+
+end
---
+++ BASELINE/BASELINE/symptoms.m
@@ -0,0 +1,13 @@
+function theta = symptoms(t)
+
+    precip = [46.7 49 45.3 33.9 22.0 13.1 10.8 16.5 42.7 61.6 70.1 58.0];
+    precip=precip-min(precip);
+    precip=precip/max(precip);
+    dryness = 1-precip;
+    
+    month = ceil(mod(t,365)*12/365);
+    if month==0; month=12; end
+    
+    theta = dryness(month);
+
+end
